d5e46ea783a8877b7b784bf4c6ad8b3bd64492e6
Himu1816 <58228141+Himu1816@users.noreply.github.com>
2025-07-09 11:49:21 +0530

    Create docker_push.groovy

---
+++ docker_push.groovy
@@ -0,0 +1,7 @@
+def call(){
+  withCredentials([usernamePassword('credentialsId': "dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+    sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
+    sh "docker push ${env.dockerHubUser}/notes-app:latest "
+  }
+}
